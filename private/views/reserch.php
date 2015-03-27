f3e80dbb6404be197779b7c9e352732cbdb08ee2
Laddamart <laddamartits@gmail.com>
2015-03-27 17:24:33 +0700

    a

--- private/views/reserch.php
+++ private/views/reserch.php
@@ -1,141 +1,98 @@
+<script src="bower_components/angularjs/angular.min.js"></script>
 <link rel="stylesheet" href="public/app/reserch/reserch.css"/>
-<div class="Tinews"></div>
-<div class="BgGR"></div>
+<script src="public/app/reserch/reserchAPP.js"></script>
+<div ng-app="reserch"
+     ng-controller="ReserchListCtl">
+    <div class="Tinews">
+        <div class="sum">
+            <div class="material">รวมเนื้อหา</div>
+            <div class="video">รวมวีดีโอ</div>
+        </div>
+        <div class="Caption">หัวข้อ
+            <a href="images/G_reserch/42585048.jpg" rel="prettyPhoto[pp_gal]"
+               title="Google.com opened at 100%">
+                <div class="Pic">
+                    <div class="TextPic">รักษาการ</div>
+                </div>
+            </a>
 
-<div class="sum">
-    <div class="material">รวมเนื้อหา</div>
-    <div class="video">รวมวีดีโอ</div>
+            <a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
+               title="Google.com opened at 100%">
+                <div class="PicII">
+                    <div class="TextPicII">รักษาการ</div>
+                </div>
+            </a>
+            <a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
+               title="Google.com opened at 100%">
+                <div class="Pix">
+                    <div class="TextPix">รักษาการ</div>
+                </div>
+                <div class="PixII">
+                    <div class="TextPixII">รักษาการ</div>
+                </div>
+            </a>
+        </div>
+
+        <div class="CaptionII">หัวข้อ
+            <a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
+               title="Google.com opened at 100%">
+                <div class="Picture">
+                    <div class="TextPicture">รักษาการ</div>
+                </div>
+            </a>
+            <a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
+               title="Google.com opened at 100%">
+                <div class="PictureII">
+                    <div class="TextPicture">รักษาการ</div>
+                </div>
+            </a>
+            <a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
+               title="Google.com opened at 100%">
+            </a>
+        </div>
+        <div class="CaptionNews">หัวข้อ
+            <div class="PictureNews">
+                <div class="TextPN">รักษาการ</div>
+            </div>
+
+            <div class="other">
+                หัวข้อ
+                    <div class="Pic_other"ng-repeat="other in others | limitTo: 4"></div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="buttonleft_small"></div>
+    <div class="buttonright_small"></div>
+
+    <div class="select">
+        <div class="block_select">
+            <div class="label_select"
+                 ng-style="{'margin-top': reserchs.length-$index}"
+                 ng-repeat="reserch in reserchs | limitTo: 5">
+            </div>
+        </div>
+        <div class="line">
+            <div class="Textline">หมวดหมู่การวิจัย</div>
+        </div>
+
+        <div class="buttonleft"></div>
+        <div class="buttonright"></div>
+        <div class="icon_grp"></div>
+        <div class="groups">
+            <div class="on"></div>
+            <div class="grps">หมวดหมู่ย่อยกรองทิพย์</div>
+            <div class="subtype"
+                 ng-repeat="subtype in subtypes | limitTo: 10">
+            </div>
+            <div class="down"></div>
+        </div>
+    </div>
+    <div ng-app="reserch"
+         ng-controller="ReserchListCtl">
+    </div>
 </div>
-<div class="Caption">หัวข้อ</div>
-<a href="images/G_reserch/42585048.jpg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="Pic">
-        <div class="TextPic">รักษาการแทนผู้อำนวยการ</div>
-    </div>
-</a>
-
-<a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="PicII">
-        <div class="TextPicII">รักษาการแทนผู้อำนวยการ</div>
-    </div>
-</a>
-
-<a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="Pix">
-        <div class="TextPix">รักษาการแทนผู้อำนวยการยาสูบ</div>
-    </div>
-    <div class="PixII">
-        <div class="TextPixII">รักษาการแทนผู้อำนวยการยาสูบ</div>
-    </div>
-</a>
-
-
-<div class="CaptionII">หัวข้อ</div>
-<a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="Picture">
-        <div class="TextPicture">รักษาการแทนผู้อำนวยการยาสูบ</div>
-    </div>
-</a>
-
-<a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="PictureII">
-        <div class="TextPicture">รักษาการแทนผู้อำนวยการยาสูบ</div>
-    </div>
-</a>
-
-<a href="images/G_reserch/Image.jpeg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="CaptionNews">หัวข้อ</div>
-    ​
-    <div class="PictureNews">
-        <div class="TextPN">รักษาการแทนผู้อำนวยการยาสูบ</div>
-    </div>
-</a>
-
-<div class="CaptionOther">หัวข้อ</div>
-<a href="images/G_reserch/Tobaccoo.jpg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="Pic_other">
-        <div class="TextPicOther">รักษาการแทนผู้อำนวยการยาสูบ</div>
-    </div>
-</a>
-
-<a href="images/G_reserch/Tobaccoo.jpg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="Pic_OtherII">
-        <div class="Text_POII">รักษาการแทนผู้อำนวยการยาสูบ</div>
-    </div>
-</a>
-
-<a href="images/G_reserch/Tobaccoo.jpg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="Pic_OtherIII">
-        <div class="Text_POIII">รักษาการแทนผู้อำนวยการยาสูบ</div>
-    </div>
-</a>
-
-<a href="images/G_reserch/Tobaccoo.jpg" rel="prettyPhoto[pp_gal]"
-   title="Google.com opened at 100%">
-    <div class="Pic_OtherIV">
-        <div class="Text_POIV">รักษาการแทนผู้อำนวยการยาสูบ</div>
-    </div>
-</a>
-
-
-<div class="buttonleft_small"></div>
-<div class="buttonright_small"></div>
-
-<div class="select">
-    <div class="Krongthip">
-        <div class="TextKt">กรองทิพย์</div>
-    </div>
-
-    <div class="ResCig">
-        <div class="TextRc">วิจัยบุหรี่</div>
-    </div>
-
-    <div class="SaiFon">
-        <div class="textSf">สายฝน</div>
-    </div>
-
-    <div class="DejaVu">
-        <div class="TextDv">เดจาวู</div>
-    </div>
-
-    <div class="develop">
-        <div class="TextDl">การพัฒนาการ<br>บุหรี่ยุคก่อนจน<br>ถึงปัจจุบัน</div>
-    </div>
-
-    <div class="line">
-        <div class="Textline">หมวดหมู่การวิจัย</div>
-    </div>
-
-    <div class="buttonleft"></div>
-    <div class="buttonright"></div>
-
-    <div class="icon_grp"></div>
-
-    <div class="groups">
-        <div class="on"></div>
-        <div class="grps">หมวดหมู่ย่อยกรองทิพย์</div>
-    </div>
-    <div class="mass">มวลบุหรี่</div>
-    <div class="pack">ซองบุหรี่</div>
-    <div class="tobacco">ใบยา</div>
-    <div class="total">ยอดขายประจำปี</div>
-    <div class="cigN">ก้นกรองแบบใหม่</div>
-    <div class="cigNe">ก้นกรองแบบใหม่</div>
-    <div class="cig">บุหรี่</div>
-    <div class="cigs">บุหรี่</div>
-    <div class="receipts">รายรับ</div>
-    <div class="receiptsII">รายรับ</div>
-    <div class="down"></div>
-</div>
-
 <script type="text/javascript" charset="utf-8">
     $(document).ready(function () {
         $("a[rel^='prettyPhoto']").prettyPhoto();
