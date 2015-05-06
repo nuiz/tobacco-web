fa0160dea9cd7969ded547dc6866ef6e1334c541
NUIZ <papangping@gmail.com>
2015-05-06 19:00:12 +0700

    000

--- private/views/expert.php
+++ private/views/expert.php
@@ -1,36 +1,42 @@
 <link rel="stylesheet" href="public/app/expert/expert.css"/>
 <script src="bower_components/angularjs/angular.min.js"></script>
 <script src="public/app/expert/expert.js"></script>
-<div class="body">
-    <div ng-app="expert"
-         ng-controller="ExpertListCtl">
-        <div class="bg_expert"></div>
-        <div class="label_ep"></div>
-        <div class="circle">
-            <a class="toggle" href="#"></a>
+<div class="expert-body" ng-app="expert"
+     ng-controller="ExpertListCtl">
+    <div class="label_ep"></div>
+    <div class="circle">
+        <div class="cat-icon cat-1">
+            <div class="yellow-line"></div>
         </div>
-        <div class="circle2" style="display: none">
-            <a class="toggle2" href="#"></a>
+        <div class="cat-icon cat-2">
+            <div class="yellow-line"></div>
         </div>
-        <div class="back_home" ng-click="homeClick()"></div>
+        <div class="cat-icon cat-3">
+            <div class="yellow-line"></div>
+        </div>
+        <div class="cat-icon cat-4">
+            <div class="yellow-line"></div>
+        </div>
+        <div class="cat-icon cat-5">
+            <div class="yellow-line"></div>
+        </div>
+        <div class="cat-icon cat-6">
+            <div class="yellow-line"></div>
+        </div>
+        <div class="cat-icon cat-7">
+            <div class="yellow-line"></div>
+        </div>
+        <div class="cat-icon cat-8">
+            <div class="yellow-line"></div>
+        </div>
+        <div class="cat-icon cat-9">
+            <div class="yellow-line"></div>
+        </div>
+        <div class="cat-icon cat-10">
+            <div class="yellow-line"></div>
+        </div>
+        <div class="cat-icon cat-11">
+            <div class="yellow-line"></div>
+        </div>
     </div>
 </div>
-<script>
-    $(function () {
-        var circle = $('.circle');
-        var circle2 = $('.circle2');
-        var toggle = $('.toggle');
-        var toggle2 = $('.toggle2');
-
-        toggle.click(function (e) {
-            e.preventDefault();
-            circle.hide();
-            circle2.show()
-        });
-        toggle2.click(function (e) {
-            e.preventDefault();
-            circle2.hide();
-            circle.show()
-        });
-    });
-</script>
