de14b02205c04f96ffe6708386fc36b5b09f4674
NuizHome <papangping@gmail.com>
2015-05-07 09:08:16 +0700

    ebook reader

--- private/views/expert.php
+++ private/views/expert.php
@@ -5,38 +5,52 @@
      ng-controller="ExpertListCtl">
     <div class="label_ep"></div>
     <div class="circle">
-        <div class="cat-icon cat-1">
+        <div class="circle-grow" ng-hide="selectedCat==null"></div>
+        <div class="lever" ng-click="clickLever()"></div>
+
+        <div class="cat-icon cat-1" ng-click="clickCat(0)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญกฎหมาย</div>
         </div>
-        <div class="cat-icon cat-2">
+        <div class="cat-icon cat-2" ng-click="clickCat(1)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญด้านการผลิต</div>
         </div>
-        <div class="cat-icon cat-3">
+        <div class="cat-icon cat-3" ng-click="clickCat(2)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญการพิมพ์</div>
         </div>
-        <div class="cat-icon cat-4">
+        <div class="cat-icon cat-4" ng-click="clickCat(3)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญการแพทย์</div>
         </div>
-        <div class="cat-icon cat-5">
+        <div class="cat-icon cat-5" ng-click="clickCat(4)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญการช่าง</div>
         </div>
-        <div class="cat-icon cat-6">
+        <div class="cat-icon cat-6" ng-click="clickCat(5)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญการวิจัย</div>
         </div>
-        <div class="cat-icon cat-7">
+        <div class="cat-icon cat-7" ng-click="clickCat(6)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญบัญชีและงบประมาน</div>
         </div>
-        <div class="cat-icon cat-8">
+        <div class="cat-icon cat-8" ng-click="clickCat(7)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญผลิตภัณฑ์</div>
         </div>
-        <div class="cat-icon cat-9">
+        <div class="cat-icon cat-9" ng-click="clickCat(8)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญบริหาร</div>
         </div>
-        <div class="cat-icon cat-10">
+        <div class="cat-icon cat-10" ng-click="clickCat(9)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญเทคโนโลยี</div>
         </div>
-        <div class="cat-icon cat-11">
+        <div class="cat-icon cat-11" ng-click="clickCat(10)">
             <div class="yellow-line"></div>
+            <div class="name">ผู้เชี่ยวชาญการ</div>
         </div>
     </div>
 </div>
--- public/app/expert/expert.js
+++ public/app/expert/expert.js
@@ -3,8 +3,28 @@
  */
 "use strict";
 var expertapp = angular.module('expert', []);
-expertapp.controller('ExpertListCtl', ['$scope', function ($scope) {
+expertapp.controller('ExpertListCtl', ['$scope', '$http', function ($scope, $http) {
     $scope.homeClick = function () {
         window.location.href = "?view=home";
-    }
+    };
+
+    $scope.cats = [];
+    $http.get("http://localhost/tobacco/guru/category").success(function(data){
+        $scope.cats = data.data;
+    });
+
+    $scope.catPage = 0;
+    $scope.clickLever = function(){
+        if($scope.catPage == 0)
+            $scope.catPage = 1;
+        else
+            $scope.catPage = 0;
+    };
+
+    $scope.selectedCat = null;
+    $scope.clickCat = function(index){
+        $scope.selectedCat = index;
+        $('.cat-icon').removeClass('selected');
+        $('.cat-icon').eq(index).addClass('selected');
+    };
 }]);
