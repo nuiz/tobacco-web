a92d4bca285fa9e6663ca8243adb417a3ca5ac8e
NUIZ <papangping@gmail.com>
2015-08-10 12:18:37 +0700

    ....

--- private/views/tp-blank.php
+++ private/views/tp-blank.php
@@ -21,8 +21,8 @@ session_start();
     <script src="public/app/config.js"></script>
 	<script>
 window.config = {
-    nfc_auth_ip: "http://<?php echo $_SERVER["HTTP_HOST"];?>:5000",
-    api_url: "http://<?php echo $_SERVER["HTTP_HOST"];?>/tobacco"
+    nfc_auth_ip: "http://58.137.91.19:5000",
+    api_url: "http://58.137.91.19/tobacco"
 };
 </script>
 </head>
--- public/app/config.js
+++ public/app/config.js
@@ -4,5 +4,5 @@
 
 window.config = {
     nfc_auth_ip: "http://58.137.91.19:5000",
-    api_url: "http://172.20.55.21/tobacco"
+    api_url: "http://58.137.91.19/tobacco"
 };
